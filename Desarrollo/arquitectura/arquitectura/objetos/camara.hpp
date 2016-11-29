58f75929bdab7d082db65f2dacb9fca3b07d5d8a
nereacastellanos <castellanosnerea@gmail.com>
2016-11-29 19:15:16 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/arquitectura' into arquitectura""
    This reverts commit 3882d2f1f4c40e2d9807078611ee6f9ddc739492.

---
+++ Desarrollo/arquitectura/arquitectura/objetos/camara.hpp
@@ -0,0 +1,20 @@
+//
+//  camara.hpp
+//  arquitectura
+//
+//  Created by Nerea Castellanos Rodríguez on 29/11/16.
+//  Copyright © 2016 Stoycho Ivanov Atanasov. All rights reserved.
+//
+
+#ifndef camara_hpp
+#define camara_hpp
+
+#include <stdio.h>
+#include "GameObject.hpp"
+
+class camara : public GameObject{
+public:
+    camara();
+    ~camara();
+};
+#endif /* camara_hpp */
---
+++ Desarrollo/arquitectura/arquitectura/objetos/puerta.cpp
@@ -0,0 +1,17 @@
+//
+//  puerta.cpp
+//  arquitectura
+//
+//  Created by Nerea Castellanos Rodríguez on 29/11/16.
+//  Copyright © 2016 Stoycho Ivanov Atanasov. All rights reserved.
+//
+
+#include "puerta.hpp"
+
+puerta::puerta(){
+    
+}
+
+puerta::~puerta(){
+    
+}
