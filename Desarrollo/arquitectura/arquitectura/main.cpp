3585ef34ca6cc2103b0cb63a715584c8637985d4
SrIvanov <stoychoia4@gmail.com>
2016-11-29 18:28:59 +0100

    Proyecto_arquitectura

---
+++ Desarrollo/arquitectura/arquitectura/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  arquitectura
+//
+//  Created by Stoycho Ivanov Atanasov on 29/11/16.
+//  Copyright © 2016 Stoycho Ivanov Atanasov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
