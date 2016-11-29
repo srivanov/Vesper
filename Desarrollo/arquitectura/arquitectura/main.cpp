6b2e6c4d3379742e418c8baa0f9a28ea85d4c8a2
SrIvanov <stoychoia4@gmail.com>
2016-11-29 19:06:07 +0100

    Revert "Revert "Revert "Merge remote-tracking branch 'origin/arquitectura' into arquitectura"""
    This reverts commit 1aec3725b899923d035fc2e122fbf50f48046432.

--- Desarrollo/arquitectura/arquitectura/main.cpp
+++ Desarrollo/arquitectura/arquitectura/main.cpp
@@ -6,14 +6,11 @@
 //  Copyright © 2016 Stoycho Ivanov Atanasov. All rights reserved.
 //
 
-<<<<<<< HEAD
 #include <iostream>
 #include "components/input.hpp"
-#include "objetos/player.hpp"
 int main(int argc, const char * argv[]) {
 	// insert code here...
 	std::cout << "Hello, World!\n";
 	input *i = new input();
-	player* pl = new player();
     return 0;
 }
