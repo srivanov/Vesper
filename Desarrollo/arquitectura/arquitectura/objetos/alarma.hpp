1aec3725b899923d035fc2e122fbf50f48046432
SrIvanov <stoychoia4@gmail.com>
2016-11-29 19:05:28 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/arquitectura' into arquitectura""
    This reverts commit 457ce07676aad197004d3ce3299d9da4d6f7459f.

---
+++ Desarrollo/arquitectura/arquitectura/objetos/alarma.hpp
@@ -0,0 +1,20 @@
+//
+//  alarma.hpp
+//  arquitectura
+//
+//  Created by Nerea Castellanos Rodríguez on 29/11/16.
+//  Copyright © 2016 Stoycho Ivanov Atanasov. All rights reserved.
+//
+
+#ifndef alarma_hpp
+#define alarma_hpp
+
+#include <stdio.h>
+#include "GameObject.hpp"
+
+class alarma : public GameObject{
+public:
+    alarma();
+    ~alarma();
+};
+#endif /* alarma_hpp */
