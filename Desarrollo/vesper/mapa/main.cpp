3f972bbaa4d93517787cebf29d6c1824a3e080de
SrIvanov <stoychoia4@gmail.com>
2016-11-30 12:54:36 +0100

    camara_inteligente_progresiva
    ZOOM

--- Desarrollo/vesper/mapa/main.cpp
+++ Desarrollo/vesper/mapa/main.cpp
@@ -43,19 +43,25 @@ int main(){
     float posYCamara = -5; //-5
     float posZCamara = -10; //-10
     float incZ = 0, incY =0, incX =0;
-    uint32_t cont = 1000;
+//    uint32_t cont = 0;
     interfaceIrr->_setFarValue(20);
-    
-    //*****************TRAZADO DE SUPERRAYOS*****************
-    ISceneCollisionManager* collMan = interfaceIrr->getSceneCollisionManager();
-    
+	interfaceIrr->_setNearValue(1);
+
+	
     interfaceIrr->_setCamPosition(new float[3]{player->_getNodePosition()[0]+posXCamara, player->_getNodePosition()[1]+posYCamara, player->_getNodePosition()[2]+posZCamara});
     interfaceIrr->_setCamTarget(new float[3]{player->_getNodePosition()[0],player->_getNodePosition()[1],player->_getNodePosition()[2]});
+	
     interfaceIrr->loadMap();
+	
     while(interfaceIrr->_Run()){
 		fisicas->update();
 		fisicas->Step(vel,pos, posmouseFinal, posmouse2);
 		player->_setNodePosition(pos);
+		
+		incX = ((player->_getNodePosition()[0]+posXCamara)-interfaceIrr->_getCamPosition()[0])/(1.8*60);
+		incY = ((player->_getNodePosition()[1]+posYCamara)-(interfaceIrr->_getCamPosition()[1]))/(1.8*60);
+		incZ = ((player->_getNodePosition()[2]+posZCamara)-(interfaceIrr->_getCamPosition()[2]))/(1.8*60);
+		
 		if(bala != NULL){
 			fisicas->getBulletPosition(posicion_bala);
 			bala->_setNodePosition(posicion_bala);
@@ -63,15 +69,6 @@ int main(){
         interfaceIrr->_beginScene(true, true);
         interfaceIrr->_drawAll();
         interfaceIrr->_endScene();
-        
-        //me creo el rayo
-//        line3d<float> rayo;
-//        rayo.start = vector3df(player2->_getNodePosition()[0],player2->_getNodePosition()[1],player2->_getNodePosition()[2]);
-//        rayo.end = vector3df(player->_getNodePosition()[0],player->_getNodePosition()[1],player->_getNodePosition()[2]);
-
-        vector3df intersection;
-        triangle3df hitTriangle;
-//        ISceneNode* selectedNodeScene = collMan->getSceneNodeAndCollisionPointFromRay(rayo, intersection, hitTriangle);
 		
 		vel[0] = 0; vel[1] = 0;
 		if(interfaceIrr->isKeyDown('Q')){
@@ -90,25 +87,15 @@ int main(){
 			vel[0] =  1;
 		}
         if(interfaceIrr->isKeyDown('O')){ //zoom lejos
-            if(posZCamara > -10){ //la segunda vez q alejamos, vuelva a alejar (NO DEBERIA)
-                //printf("Z: %f \n", posZCamara);
-                incX = ((player->_getNodePosition()[0]+posXCamara)-interfaceIrr->_getCamPosition()[0])/(2*60);
-                incZ = ((interfaceIrr->_getCamPosition()[2]-5)-(interfaceIrr->_getCamPosition()[2]))/(2*60);
-                incY = ((interfaceIrr->_getCamPosition()[1]-2.5)-(interfaceIrr->_getCamPosition()[1]))/(2*60);
-                
-//            incZ = ((player->_getNodePosition()[2]+5)-(player->_getNodePosition()[2]))/(2*60);
-//            incY = ((player->_getNodePosition()[1]+5)-(player->_getNodePosition()[1]))/(2*60);
-                cont = 0;
-            }
+			posYCamara = -5;
+			posZCamara = -10;
+//			cont = 0;
         }
         if(interfaceIrr->isKeyDown('P')){ //zoom cerca
-            if(posZCamara < -5){
-                incX = ((player->_getNodePosition()[0]+posXCamara)-interfaceIrr->_getCamPosition()[0])/(2*60);
-                incZ = ((interfaceIrr->_getCamPosition()[2]+5)-(interfaceIrr->_getCamPosition()[2]))/(2*60);
-                incY = ((interfaceIrr->_getCamPosition()[1]+2.5)-(interfaceIrr->_getCamPosition()[1]))/(2*60);
-   
-                cont = 0;
-            }
+			posYCamara = -2.5;
+			posZCamara = -5;
+//			cont = 0;
+
         }
 		if(interfaceIrr->isKeyDown('L')){
 			vel[0] = 0; vel[1] = 0;
@@ -120,19 +107,20 @@ int main(){
 			}
 		}
         
-        if(cont < 120){
+//        if(cont < 120){
             interfaceIrr->_setCamPosition(new float[3]{interfaceIrr->_getCamPosition()[0] + incX,interfaceIrr->_getCamPosition()[1] + incY, interfaceIrr->_getCamPosition()[2] + incZ});
-            interfaceIrr->_setCamTarget(new float[3]{player->_getNodePosition()[0],player->_getNodePosition()[1],player->_getNodePosition()[2]});
-
-            posXCamara = interfaceIrr->_getCamPosition()[0] - player->_getNodePosition()[0];
-            posYCamara = interfaceIrr->_getCamPosition()[1] - player->_getNodePosition()[1];
-            posZCamara = interfaceIrr->_getCamPosition()[2] - player->_getNodePosition()[2];
-            cont++;
-        }else{
-          //  para que la camara se mueva en funcion de las teclas pulsadas
-            interfaceIrr->_setCamPosition(new float[3]{player->_getNodePosition()[0]+posXCamara, player->_getNodePosition()[1]+posYCamara, player->_getNodePosition()[2]+posZCamara});
-            interfaceIrr->_setCamTarget(new float[3]{player->_getNodePosition()[0],player->_getNodePosition()[1],player->_getNodePosition()[2]});
-        }
+//            interfaceIrr->_setCamTarget(new float[3]{player->_getNodePosition()[0],player->_getNodePosition()[1],player->_getNodePosition()[2]});
+			interfaceIrr->_setCamTarget(new float[3]{interfaceIrr->_getCamPosition()[0], (float)(interfaceIrr->_getCamPosition()[1]+2.5), interfaceIrr->_getCamPosition()[2]+5});
+//            posXCamara = interfaceIrr->_getCamPosition()[0] - player->_getNodePosition()[0];
+//            posYCamara = interfaceIrr->_getCamPosition()[1] - player->_getNodePosition()[1];
+//            posZCamara = interfaceIrr->_getCamPosition()[2] - player->_getNodePosition()[2];
+//            cont++;
+//        }else{
+//            para que la camara se mueva en funcion de las teclas pulsadas
+//
+//            interfaceIrr->_setCamPosition(new float[3]{player->_getNodePosition()[0]+posXCamara, player->_getNodePosition()[1]+posYCamara, player->_getNodePosition()[2]+posZCamara});
+//            interfaceIrr->_setCamTarget(new float[3]{player->_getNodePosition()[0],player->_getNodePosition()[1],player->_getNodePosition()[2]});
+//        }
     }
     interfaceIrr->_deviceClose();
 }
