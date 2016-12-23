927c9e15cb9cba5f78c8e6b020d5b5dd84a0ce54
SrIvanov <stoychoia4@gmail.com>
2016-12-23 17:47:14 +0100

    pintando poligono

--- Desarrollo/TAG/motor/motor/main.cpp
+++ Desarrollo/TAG/motor/motor/main.cpp
@@ -6,7 +6,7 @@
 
 #include <GLFW/glfw3.h>
 
-// Window dimensions
+// dimensiones de la ventana
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 // Shaders
@@ -25,6 +25,20 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
 	"}\0";
 
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// detectamos la pulsacion de la tecla
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		// hacemos que se salga del bucle
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	
+	if(key == GLFW_KEY_D && action == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
+	if(key == GLFW_KEY_F && action == GLFW_PRESS)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
+
 int main(int argc, const char * argv[]) {
 
 	glfwInit();
@@ -34,7 +48,7 @@ int main(int argc, const char * argv[]) {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	
-	// Create a GLFWwindow object that we can use for GLFW's functions
+	// creamos una ventana con glfw
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
 	if (window == nullptr)
 	{
@@ -42,19 +56,23 @@ int main(int argc, const char * argv[]) {
 		glfwTerminate();
 		return -1;
 	}
+	//seleccionamos window para que se haga la gestion sobre esta ventana
 	glfwMakeContextCurrent(window);
 	
-	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	//definimos la funcion que llamaremos cuando se pulse una tecla
+	glfwSetKeyCallback(window, key_callback);
+	
+	// activamos esto para que GLEW pueda trabajar con punteros de forma moderna, algo asi
 	glewExperimental = GL_TRUE;
 	
-	// Initialize GLEW to setup the OpenGL Function pointers
+	// inicializamos GLEW
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
 	
-	// Define the viewport dimensions
+	// definimos el viewport, sera el area en el que dibujamos
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
@@ -117,16 +135,23 @@ int main(int argc, const char * argv[]) {
 	
 	
 	GLfloat vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
+		0.5f,  0.5f, 0.0f,  // Top Right
+		0.5f, -0.5f, 0.0f,  // Bottom Right
+		-0.5f, -0.5f, 0.0f,  // Bottom Left
+		-0.5f,  0.5f, 0.0f   // Top Left
+		
+	};
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3,   // First Triangle
+		1, 2, 3    // Second Triangle
 	};
 	
 	//genera el vertex buffer
-	GLuint VBO, VAO;
+	GLuint VBO, VAO, EBO;
 	//generamos el Vertex Array Object
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	//linkamos el VAO
 	glBindVertexArray(VAO);
 	
@@ -135,6 +160,9 @@ int main(int argc, const char * argv[]) {
 	
 	//copiamos los datos de los vertices a buffer GL_ARRAY_BUFFER
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
 	/* aqui le decimos a OpenGL como interpretar el buffer de vertices cuando tenga que dibujar
 	 * el primer parametro es el indice del vertice que queremos configurar
@@ -172,8 +200,11 @@ int main(int argc, const char * argv[]) {
 		//linkamos el VAO
 		glBindVertexArray(VAO);
 		
-		//dibuja el array de vertices
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+//		//dibuja el array de vertices
+//		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 		
 		//deslinkamos el VAO por seguridad
 		glBindVertexArray(0);
@@ -181,10 +212,13 @@ int main(int argc, const char * argv[]) {
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
-	
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
 	
 	return 0;
 }
+
 
