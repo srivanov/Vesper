f73ceb14f0c6703101f0fa9f19336eabbec1b28c
SrIvanov <stoychoia4@gmail.com>
2016-12-23 03:17:38 +0100

    pintando triangulo

--- Desarrollo/TAG/motor/motor/main.cpp
+++ Desarrollo/TAG/motor/motor/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 #define GLEW_STATIC
@@ -8,6 +9,22 @@
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 position;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
+
 int main(int argc, const char * argv[]) {
 
 	glfwInit();
@@ -16,7 +33,6 @@ int main(int argc, const char * argv[]) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	// hola
 	
 	// Create a GLFWwindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
@@ -27,8 +43,10 @@ int main(int argc, const char * argv[]) {
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	
 	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
 	glewExperimental = GL_TRUE;
+	
 	// Initialize GLEW to setup the OpenGL Function pointers
 	if (glewInit() != GLEW_OK)
 	{
@@ -46,13 +64,109 @@ int main(int argc, const char * argv[]) {
 		0.5f, -0.5f, 0.0f,
 		0.0f,  0.5f, 0.0f
 	};
+	
 	//genera el vertex buffer
-	GLuint VBO;
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+	
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
+	
+	
 	//linkamos el buffer GL_ARRAY_BUFFER al puntero VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
 	//copiamos los datos de los vertices a buffer GL_ARRAY_BUFFER
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	//creamos un vertex shader y recogemos su ID
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	
+	//compilamos el vertex shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	
+	//comprobamos si se ha compilado con exito
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	
+	if(!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	
+	//compilamos el fragment shader
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	
+	//comprobamos si se ha compilado con exito
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	
+	if(!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	
+	//creamos el programa de shaders
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	
+	//linkamos los shaders al programa y activamos este para que se use en el render
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	
+	//comprobamos que se ha linkado con exito
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+	}
+	
+	//activamos el programa para renderizar
+	glUseProgram(shaderProgram);
+	
+	//borramos los shader ya que no los vamos a utilizar mas
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	/* aqui le decimos a OpenGL como interpretar el buffer de vertices cuando tenga que dibujar
+	 * el primer parametro es el indice del vertice que queremos configurar
+	 * el segundo es el numero de ejes que contiene cada vertice, 1 2 3 o 4
+	 * el tercero especifica el tipo de dato que vamos a manejar
+	 * el cuarto indica si queremos normalizar los valores o no
+	 * el quinto especifica el tamaño de que tiene cada vertice en el buffer, ahora son 3 floats
+	 * el sexto es un puntero a la posicion que queremos empezar la figura, esto sirve para cuando tengamos
+	 * mas de uno
+	 *
+	 * en la otra funcion activamos los atributos de vertices ya que estan desactivados por defecto
+	 */
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	
+	
+	
+	
 	
 	
 	
@@ -61,8 +175,14 @@ int main(int argc, const char * argv[]) {
 	{
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
 		glfwPollEvents();
-//		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-//		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 3, GL_FLOAT, 0);
+		glBindVertexArray(0);
+		
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
@@ -72,3 +192,4 @@ int main(int argc, const char * argv[]) {
 	
 	return 0;
 }
+
