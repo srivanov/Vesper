e2210790f0eeed5d0d6c6d4e056e62071c946f57
SrIvanov <stoychoia4@gmail.com>
2017-01-02 12:16:58 +0100

    primera textura

--- Desarrollo/TAG/motor/Shaders/basico.frag
+++ Desarrollo/TAG/motor/Shaders/basico.frag
@@ -2,9 +2,14 @@
 #version 330 core
 
 in vec3 ourColor;
+in vec2 TexCoord;
+
 out vec4 color;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-	color = vec4(ourColor, 1.0f);
+	//color = vec4(ourColor, 1.0f);
+	color = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0f);
 }
--- Desarrollo/TAG/motor/motor/main.cpp
+++ Desarrollo/TAG/motor/motor/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <math.h>
 #include "Shader.h"
+#include <SOIL/SOIL.h>
 
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -70,15 +71,16 @@ int main(int argc, const char * argv[]) {
 	
 	
 	GLfloat vertices[] = {
-		// Positions         // Colors
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // Bottom Right
-	   -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // Bottom Left
-		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // Top
+		// Positions          // Colors           // Texture Coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left
 		
 	};
 	GLuint indices[] = {  // Note that we start from 0!
-		0, 1, 2//,   // First Triangle
-		//1, 2, 3    // Second Triangle
+		0, 1, 3,   // First Triangle
+		1, 2, 3    // Second Triangle
 	};
 	
 	//genera el vertex buffer
@@ -99,23 +101,26 @@ int main(int argc, const char * argv[]) {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	
-	/* aqui le decimos a OpenGL como interpretar el buffer de vertices cuando tenga que dibujar
-	 * el primer parametro es el indice del vertice que queremos configurar
-	 * el segundo es el numero de ejes que contiene cada vertice, 1 2 3 o 4
-	 * el tercero especifica el tipo de dato que vamos a manejar
-	 * el cuarto indica si queremos normalizar los valores o no
-	 * el quinto especifica el tamaño de que tiene cada vertice en el buffer, ahora son 3 floats
-	 * el sexto es un puntero a la posicion que queremos empezar la figura, esto sirve para cuando tengamos
-	 * mas de uno
-	 *
-	 * en la otra funcion activamos los atributos de vertices ya que estan desactivados por defecto
-	 */
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	/*
+	* aqui le decimos a OpenGL como interpretar el buffer de vertices cuando tenga que dibujar
+	* el primer parametro es el indice del vertice que queremos configurar
+	* el segundo es el numero de ejes que contiene cada vertice, 1 2 3 o 4
+	* el tercero especifica el tipo de dato que vamos a manejar
+	* el cuarto indica si queremos normalizar los valores o no
+	* el quinto especifica el tamaño de que tiene cada vertice en el buffer, ahora son 3 floats
+	* el sexto es un puntero a la posicion que queremos empezar la figura, esto sirve para cuando tengamos mas de uno
+	* en la otra funcion activamos los atributos de vertices ya que estan desactivados por defecto
+	*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	
 	// atributos del color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3* sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	
+	// atributos de las texturas
+	glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 	
 	//linkamos el buffer por su ID
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -123,7 +128,35 @@ int main(int argc, const char * argv[]) {
 	//deslinkamos el VAO por seguridad
 	glBindVertexArray(0);
 	
+	//cargamos la imagen desde el archivo
+	int tex_width, tex_height;
+	unsigned char* image = SOIL_load_image("../Textures/container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
 	
+	//generamos la textura, se pueden crear varias a la vez cambiando el primer parametro de la funcion
+	GLuint texture;
+	glGenTextures(1, &texture);
+	
+	//linkamos la textura al objeto en el primer parametro
+	glBindTexture(GL_TEXTURE_2D, texture);
+	
+	/*
+	 * asignamos la imagen a la textura de OpenGL
+	 * el primer argumento especifica a que textura queremos asignar la imagen
+	 * el segundo indica el nivel de mipmap, se pone a 0 porque las generamos automaticamente en la siguiente linea
+	 * el tercero indica el tipo en el que queremos almacenar la textura
+	 * el quinto y sexto indica el ancho y alto de la imagen
+	 * el siguiente siempre tiene que ser 0
+	 * el septimo y octavo especifican el tipo y el formato de la imagen fuente cargada
+	 * el ultimo es la imagen
+	 */
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	
+	//hacemos que OpenGL genere el mipmap de la textura automaticamente
+	glGenerateMipmap(GL_TEXTURE_2D);
+	
+	//liberamos la memoria de la imagen y deslinkamos el objeto de textura
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
 	
 	// Bucle principal
 	while (!glfwWindowShouldClose(window))
@@ -138,6 +171,9 @@ int main(int argc, const char * argv[]) {
 //		//activamos el programa para renderizar
 //		glUseProgram(shaderProgram);
 		miShader.Use();
+		
+		//linkamos la textura para dibujarla
+		glBindTexture(GL_TEXTURE_2D, texture);
 		
 		//linkamos el VAO
 		glBindVertexArray(VAO);
