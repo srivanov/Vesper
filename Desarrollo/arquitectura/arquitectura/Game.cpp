546cb27167fa9445d1dad448ed0ca3d8d19b95e6
nereacastellanos <castellanosnerea@gmail.com>
2017-01-29 12:26:57 +0100

    cargando mapa 2

--- Desarrollo/arquitectura/arquitectura/Game.cpp
+++ Desarrollo/arquitectura/arquitectura/Game.cpp
@@ -46,7 +46,7 @@ void Game::start(uint32_t ancho, uint32_t alto, uint32_t color, bool fullscreen,
 	dvector3D campos(jpos.x, jpos.y - 5, jpos.z - 10);
 	cam->addCamara(campos, *jugador->getPosicion());
 	
-	if(nivelazo->cargarNivel("1"))
+	if(nivelazo->cargarNivel("2"))
 		nivelazo->dibujarMapa();
 	
 	World_BlackBoard::instance();
