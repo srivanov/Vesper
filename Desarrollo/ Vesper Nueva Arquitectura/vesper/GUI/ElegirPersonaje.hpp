8ceb6babb5d92e2ee93abbfbab709332e1260b26
nereacastellanos <castellanosnerea@gmail.com>
2017-04-02 20:54:25 +0200

    "integrando" nuevos layouts

---
+++ Desarrollo/ Vesper Nueva Arquitectura/vesper/GUI/ElegirPersonaje.hpp
@@ -0,0 +1,30 @@
+//
+//  ElegirPersonaje.hpp
+//  vesper
+//
+//  Created by Nerea Castellanos Rodríguez on 2/4/17.
+//  Copyright © 2017 Stoycho Ivanov Atanasov. All rights reserved.
+//
+
+#ifndef ElegirPersonaje_hpp
+#define ElegirPersonaje_hpp
+
+#include <stdio.h>
+#include "GUI.h"
+#include "../MyEventReceiver.hpp"
+
+class ElegirPersonaje : public Motor::GUI{
+public:
+    ElegirPersonaje();
+    ~ElegirPersonaje();
+    tLayout getLayout() override;
+    void onClickVolver(const CEGUI::EventArgs &e);
+    void onClickJugar(const CEGUI::EventArgs &e);
+private:
+    CEGUI::DefaultWindow *label_elegirpersonaje;
+    CEGUI::PushButton *button_volver;
+    CEGUI::PushButton *button_jugar;
+};
+
+
+#endif /* ElegirPersonaje_hpp */
