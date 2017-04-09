7bb070434682afa30806351a281824abe64df131
SrIvanov <stoychoia4@gmail.com>
2017-04-09 15:42:23 +0200

    borrado proyecto

--- Desarrollo/Vesper/vesper/objetos/PhysicObjects/Actores/Rehen.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Rehen.cpp
-//  Arquitectura
-//
-//  Created by Gaspar Rodriguez Valero on 16/3/17.
-//  Copyright © 2017 Gaspar Rodriguez Valero. All rights reserved.
-//
-
-#include "Rehen.hpp"
