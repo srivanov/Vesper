61110a124e28a9e4f4f78cbb84202c01212797af
Juls <juliamv4b@gmail.com>
2017-04-21 10:05:05 +0200

    limpieza

--- Desarrollo/GUI_sfml/integracion_sfml/HUD.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  HUD.cpp
-//  integracion_sfml
-//
-//  Created by Catherine Castrillo González on 17/01/17.
-//  Copyright © 2017 Catherine Castrillo González. All rights reserved.
-//
-
-#include "HUD.hpp"
