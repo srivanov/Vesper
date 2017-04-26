4194ad0a0e80ab1bf98b35e135656f8a10fb64a0
Gaspar Rodriguez Valero <gaspar.rodriguez1992@gmail.com>
2017-04-26 15:47:00 +0200

    Nivel
    Inicios

---
+++ Desarrollo/VesperUnity/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Vector3 mov;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
