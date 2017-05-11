6cb77b5dbde2dd00fe0088e48a911df15c9a3e90
Juls <juliamv4b@gmail.com>
2017-05-11 12:39:43 +0200

    pruebas falla crear csv

--- Documentos/NM/id_games1.R
+++ Documentos/NM/id_games1.R
@@ -2,20 +2,23 @@ library("httr")
 library("jsonlite")
 #query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=id&offset=2&limit=20"
 #query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/people/?fields=id&offset=2&limit=15"
-#query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/people/?fields=*"
-query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/genres/?fields=name&limit=50"
-  
-
+query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/people/?fields=*"
+#query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/characters/?fields=name&limit=50"
+#query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/characters/?fields=id&name&created_at&updated_at&slug&url&people&games&limit=50"
+#query <- "https://igdbcom-internet-game-database-v1.p.mashape.com/characters/?fields=*&limit=10"
 
 r <- GET(url=query,
          add_headers(.headers = c('X-Mashape-Key' =  "l75yqzAZyVmshBB70018LMfrb7vMp15hRk5jsnLPK9ZNp4iMp1" , 
                                   Accept =   "application/json")))
-#print(r)
+print(r)
 doc<-fromJSON(content(r,type="text"))
 print(doc)
 
-file.create("prueba.csv")
-#write.csv(r, 'miDoc.csv')
-#notas<-read.table(doc)
-write.table(doc, file = "prueba.csv")
+file.create("character.csv")
+#write.csv(r, 'miDoc.csv') comentado
+#notas<-read.table(doc) comentado
+write.table(doc, file = "character.csv")
+#write.table(doc, file = "character.csv", append = TRUE, sep = ";", dec = ",",quote = FALSE,col.names = NA, qmethod = "double")
+
+
 
