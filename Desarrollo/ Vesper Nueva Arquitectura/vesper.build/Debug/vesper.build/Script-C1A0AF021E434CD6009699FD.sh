b23c599f0edcbeeb7f1052a99bea3ffa51ca9333
Catherine Castrillo González <mcatherinecg@gmail.com>
2017-05-11 12:48:45 +0200

    esqueleto bien puesto?

--- Desarrollo/ Vesper Nueva Arquitectura/vesper.build/Debug/vesper.build/Script-C1A0AF021E434CD6009699FD.sh
+++ Desarrollo/ Vesper Nueva Arquitectura/vesper.build/Debug/vesper.build/Script-C1A0AF021E434CD6009699FD.sh
@@ -18,11 +18,24 @@ export DYLIB=libglew_d.dylib
 cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
 install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
 
+<<<<<<< Updated upstream
+=======
+export DYLIB=libCEGUIOpenGLRenderer-0_d.dylib
+cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
+install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
+>>>>>>> Stashed changes
 
 export DYLIB=libglfw_d.dylib
 cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
 install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
 
+<<<<<<< Updated upstream
+=======
+export DYLIB=libCEGUIFreeImageImageCodec_d.dylib
+cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
+install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
+
+>>>>>>> Stashed changes
 export DYLIB=libjpeg_d.dylib
 cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
 install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
